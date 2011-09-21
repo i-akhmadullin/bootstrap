9bbc35810597b8020408344729255cefaf04f31a
Akhmadullin Ilya <ilya.akhmadullin@gmail.com>
2011-09-22 00:18:20 +0600

    Added base layout, css, js

---
+++ js/common.js
@@ -0,0 +1,56 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+function deserialize(str) {
+    var href = str,
+        last_index = href.lastIndexOf("?"),
+        result = {};
+    if (last_index > -1) {
+        href = href.substr(last_index + 1);
+    }
+    var data = href.split("&")
+    for (var i = 0; i < data.length; i++) {
+        if (!data[i]) continue;
+        var pair = decodeURIComponent(data[i]).split("=");
+        if (pair.length != 2) continue;
+        var _name = pair[0];
+        var value = pair[1];
+        result[_name] = value;
+    }
+    return result;
+}
+$.ajaxSetup({
+	cache: false,
+	type: 'POST',
+	dataType: 'html',
+	beforeSend: function(xhr, settings) {
+		if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+			xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+		}
+	}
+});
+
+$(document).ready(function() {
+	$("#RotatorWraper").rotator({
+		'items': '.RotatorItem',
+		'prev': '#LeftRotatorArrow',
+		'next': '#RightRotatorArrow',
+		'visibleCount': 1,
+		'changeCount' : 1,
+		'useSwipeTouch' : true,
+		/*'useHashUrl' : true,*/
+		'hashPrefix' : "slider"
+	});  
+});
